a4c3abaa2c2f55c495f67bf43ae1b690f689196b
Luis Carrero <111160964+LuisG199@users.noreply.github.com>
2023-02-24 10:44:38 -0300

    Update 3 - 🕰️_The_Model.py

--- pages/3 - 🕰️_The_Model.py
+++ pages/3 - 🕰️_The_Model.py
@@ -9,29 +9,4 @@ import pickle
 st.set_page_config(page_title="The model", page_icon=None,
                    layout="wide", initial_sidebar_state="auto",
                    menu_items=None)
-st.sidebar.title("4. Forecast")
 
-# Choose whether or not to do future forecasts
-make_future_forecast = st.sidebar.checkbox(
-    "Make forecast on future dates", value=False, help=readme["tooltips"]["choice_forecast"]
-)
-if make_future_forecast:
-    with st.sidebar.expander("Horizon", expanded=False):
-        dates = input_forecast_dates(df, dates, resampling, config, readme)
-    with st.sidebar.expander("Regressors", expanded=False):
-        datasets = input_future_regressors(
-            datasets, dates, params, dimensions, load_options, date_col
-        )
-# Launch training & forecast
-if st.checkbox(
-    "Launch forecast",
-    value=False,
-    help=readme["tooltips"]["launch_forecast"],
-):
-
-    if not (evaluate | make_future_forecast):
-        st.error("Please check at least 'Evaluation' or 'Forecast' in the sidebar.")
-
-    track_experiments = st.checkbox(
-        "Track experiments", value=False, help=readme["tooltips"]["track_experiments"]
-    )
