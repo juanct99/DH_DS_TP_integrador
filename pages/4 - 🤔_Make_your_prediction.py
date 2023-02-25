f5a11ccc8903619e09f6b283eb5b3d7543b3d0a2
Luis Carrero <111160964+LuisG199@users.noreply.github.com>
2023-02-25 14:29:34 -0300

    Update 4 - 🤔_Make_your_prediction.py

--- pages/4 - 🤔_Make_your_prediction.py
+++ pages/4 - 🤔_Make_your_prediction.py
@@ -5,6 +5,14 @@ import seaborn as sns
 import pickle
 import datetime as dt
 
+
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+from plotly.subplots import make_subplots
+from prophet import Prophet
+from prophet.plot import plot_plotly
+from prophet.plot import add_changepoints_to_plot
+
 #------------------------ Make your prediction ------------------------#
 
 st.set_page_config(page_title="Predict", page_icon=None,
@@ -58,3 +66,86 @@ variables = {
 }
 
 st.write(variables)
+
+
+#------------------------ The model------------------------#
+
+#-------Pickle----#
+
+with open("data/model_fb.pkl", 'rb') as Prophet_model_fb:
+        model_fb = pickle.load(Prophet_model_fb)
+    
+future_pd = model_fb.make_future_dataframe(
+    periods = 42,
+    freq = 'm',
+    include_history=True
+)
+
+predictions_fb = model_fb.predict(future_pd)
+
+# predict over the dataset
+predictions_fb = model_fb.predict(future_pd)
+
+
+#---grafico genial----#
+def predictgrapht(modelo,fcst):
+    fig = plot_plotly(modelo,fcst,
+            ylabel='total',
+            changepoints=False,
+            trend=True,
+            uncertainty=True,
+        )
+
+    #Load data
+    df = predictions_fb
+
+    # Create figure
+
+    fig.add_trace(
+        go.Scatter(x=list(df.ds), y=list(df.trend)))
+    #fig.add_trace(
+        #go.Scatter(x=list(df.ds), y=list(df.yhat)))
+
+    # Set title
+    fig.update_layout(
+        title_text="Time series with range slider and selectors"
+    )
+
+    # Add range slider
+    fig.update_layout(
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1,
+                         label="1m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=6,
+                         label="6m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=1,
+                         label="YTD",
+                         step="year",
+                         stepmode="todate"),
+                    dict(count=1,
+                         label="1y",
+                         step="year",
+                         stepmode="backward"),
+                    dict(step="all")
+                ])
+            ),
+            rangeslider=dict(
+                visible=True
+            ),
+            type="date"
+        )
+    )
+
+    fig.show()
+    return fig
+
+container = st.container()
+container.plotly_chart(predictgrapht(model_fb,predictions_fb), use_container_width=True, sharing="streamlit", theme="streamlit")
+
+ 
