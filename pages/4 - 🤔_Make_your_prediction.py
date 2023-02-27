0a22c2cec644eaded7e848fecac510cf7a8ce6d2
juancruz.t <juancruz.t@noduscompany.com>
2023-02-27 13:03:50 -0300

    realizo predicciones +1, -1h

--- pages/4 - 🤔_Make_your_prediction.py
+++ pages/4 - 🤔_Make_your_prediction.py
@@ -50,9 +50,9 @@ feriados = getFeriados(feriados, sep = ";")
 
 st.header("🚇Predice que tan concurrido estará el subte al momento de tu viaje")
 
+st.sidebar.subheader("Variables de entrada")
 def sidebar_form():
-    st.sidebar.subheader("Variables de entrada")
-    fecha = st.sidebar.date_input("Fecha")
+    fecha = st.sidebar.date_input("Fecha", key="date_input")
     hora = st.sidebar.selectbox('Hora',horas, index=8)
     linea = st.sidebar.selectbox('Linea',estaciones_y_lineas.linea.unique().tolist())
     estacion = estaciones_y_lineas[estaciones_y_lineas.linea == linea].estacion.sort_values().tolist()
@@ -62,16 +62,20 @@ def sidebar_form():
     
     st.sidebar.write("")
     boton = st.sidebar.button("Hacer predicción", use_container_width=True, type="primary")
-
     hour_mask = df.hora == hora
+    hour_mask_plus_1 = df.hora == hora +1 if hora != 23 else df.hora == 0
+    hour_mask_minus_1 = df.hora == hora -1 if hora != 0 else df.hora == 23
     linea_mask = df.linea == linea
     estaciones_mask = df.estacion == estacion
     sentido_mask = (df.sentido == sentido) | (df.sentido == "-")
+    
     df_filtrado = df[hour_mask & linea_mask & estaciones_mask & sentido_mask]
+    df_filtrado_plus_1 = df[hour_mask_plus_1 & linea_mask & estaciones_mask & sentido_mask]
+    df_filtrado_minus_1 = df[hour_mask_minus_1 & linea_mask & estaciones_mask & sentido_mask]
             
-    return fecha, boton, df_filtrado
+    return fecha, hora, boton, df_filtrado, df_filtrado_plus_1, df_filtrado_minus_1
 
-fecha_prediccion, boton, df_filtrado = sidebar_form()
+fecha_prediccion, hour, boton, df_filtrado, df_filtrado_plus_1, df_filtrado_minus_1 = sidebar_form()
 
 def predict(df_filtrado, fecha_prediccion):
 
@@ -97,18 +101,75 @@ def predict(df_filtrado, fecha_prediccion):
     return model, prediction
 
 if boton:
-    
-    try:
-        model, prediction = predict(df_filtrado, fecha_prediccion)
-        st.success("Predicción realizada con éxito")
-        st.dataframe(prediction.head())
-        
-        st.plotly_chart(plot_plotly(model, prediction, trend = True), use_container_width=True)
-        st.plotly_chart(plot_components_plotly(model, prediction), use_container_width=True)
-        
-        
-    except Exception as e:
-        st.exception(f"Error: {e}")
+    with st.spinner('Realizando predicciones...'):
+        try:
+            model, prediction = predict(df_filtrado, fecha_prediccion)
+            model_plus_1, prediction_plus_1 = predict(df_filtrado_plus_1, fecha_prediccion)
+            model_minus_1, prediction_minus_1 = predict(df_filtrado_minus_1, fecha_prediccion)
+            
+            st.success("Predicciones realizadas con éxito")
+            st.write("")
+            
+            def pasajeros_metrics(prediccion):
+                center = int(prediccion[prediccion.ds == str(fecha_prediccion)].yhat.values[0]) if int(prediccion[prediccion.ds == str(fecha_prediccion)].yhat.values[0]) > 0 else 0
+                limite_superior = int(prediccion[prediccion.ds == str(fecha_prediccion)].yhat_upper.values[0])
+                limite_inferior = int(prediccion[prediccion.ds == str(fecha_prediccion)].yhat_lower.values[0]) if int(prediccion[prediccion.ds == str(fecha_prediccion)].yhat_lower.values[0]) > 0 else 0
+
+                return limite_inferior, center, limite_superior
+            
+            inf, center, sup = pasajeros_metrics(prediction)
+            inf_plus_1, center_plus_1, sup_plus_1 = pasajeros_metrics(prediction_plus_1)
+            inf_minus_1, center_minus_1, sup_minus_1 = pasajeros_metrics(prediction_minus_1)
+            
+            def card_text(card_text, color):
+                card = f"""
+                <div style="border-radius: 10px; background-color: {color}; padding: 20px; display: inline-block;">
+                    <h3 ;style="font-size: 50px;">{card_text}</h3>
+                </div>
+                """
+                return card
+            
+            def tarjetas(inf, center, sup):
+                c1, c2, c3 = st.columns(3)
+                with c1:
+                    st.write(card_text(inf, "#D9FFDF"), unsafe_allow_html=True)
+                with c2:
+                    st.write(card_text(center,"#FFFECE"), unsafe_allow_html=True)
+                with c3:    
+                    st.write(card_text(sup,"#FFD1D0"), unsafe_allow_html=True)
+            
+            c1, c2, c3 = st.columns(3)
+            with c1:
+                st.subheader(":green[Límite inferior]")
+            with c2:
+                st.subheader(":black[Predicción]")
+            with c3:
+                st.subheader(":red[Límite superior]")
+            
+            st.write("")
+            st.subheader(f"{hour-1}:00 hs")
+            tarjetas(inf_minus_1, center_minus_1, sup_minus_1)
+            st.write("")
+            st.write("")
+            st.write("")
+            
+            st.write("")
+            st.subheader(f"Hora seleccionada, {hour}:00 hs")
+            tarjetas(inf, center, sup)
+            st.write("")
+            st.write("")
+            st.write("")
+            
+            st.write("")
+            st.subheader(f"{hour+1}:00 hs")
+            tarjetas(inf_plus_1, center_plus_1, sup_plus_1)
+            st.write("")
+            st.write("")
+            st.write("")
+            
+        except Exception as e:
+            st.exception(f"Error: {e}")
+
     
 else:
     st.info("Configura las variables de entrada y haz click en 'Hacer predicción' para ver los resultados")
